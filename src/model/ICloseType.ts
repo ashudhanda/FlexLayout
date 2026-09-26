0e74054a4ca8111c92120b2494193de34a2ffa1d
Ashu <ashudhanda33@gmail.com>
2026-09-26 06:27:51 +0530

    docs: add JSDoc to ICloseType enum

--- src/model/ICloseType.ts
+++ src/model/ICloseType.ts
@@ -1,5 +1,9 @@
+/** When the close ("x") button appears on a tab button or tabset header. */
 export enum ICloseType {
-    Visible = 1, // close if selected or hovered, i.e. when x is visible (will only close selected on mobile, where css hover is not available)
-    Always = 2, // close always (both selected and unselected when x rect tapped e.g where a custom image has been added for close)
-    Selected = 3, // close only if selected
+    /** close if selected or hovered, i.e. when x is visible (will only close selected on mobile, where css hover is not available) */
+    Visible = 1,
+    /** close always (both selected and unselected when x rect tapped e.g where a custom image has been added for close) */
+    Always = 2,
+    /** close only if selected */
+    Selected = 3,
 }
